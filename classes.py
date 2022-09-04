b36938838504604feea81d66f288fa5acb3d1d06
Ren You <roadtoti10@gmail.com>
2022-09-04 23:13:19 +0930

    normalized 0-1

--- classes.py
+++ classes.py
@@ -13,20 +13,11 @@ class Dataset(torch.utils.data.Dataset):
         self.main_dir = main_dir
         # self.transform = transform
         self.total_imgs = natsort.natsorted(os.listdir(main_dir))
-        print(self.total_imgs)
+        self.list_IDs = []
 
     def __len__(self):
-        # 'Denotes the total number of samples'
         return len(self.total_imgs)
 
-    # def __getitem__(self, index):
-    #     # 'Generates one sample of data'
-    #     # Select sample
-    #     ID = self.list_IDs[index]
-    #
-    #     # Load data and get label
-    #     X = torch.load('data/' + ID + '.pt')
-    #     y = self.labels[ID]
-    #
-    #     return X, y
+    def __getitem__(self, index):
+        return self.total_imgs[index]
 
--- pytorch_script.py
+++ pytorch_script.py
@@ -14,15 +14,15 @@ from torchvision.datasets import MNIST
 
 if __name__ == "__main__":
     # CUDA
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
-    torch.backends.cudnn.benchmark = True
+    # use_cuda = torch.cuda.is_available()
+    # device = torch.device("cuda:0" if use_cuda else "cpu")
+    # torch.backends.cudnn.benchmark = True
 
     # Parameters
-    params = {'batch_size': 64,
-              'shuffle': True,
-              'num_workers': 6}
-    max_epochs = 100
+    # params = {'batch_size': 64,
+    #           'shuffle': True,
+    #           'num_workers': 6}
+    # max_epochs = 100
 
     # Datasets
     data = Dataset("data/Raphael")
@@ -33,10 +33,22 @@ if __name__ == "__main__":
     # print(imageset.shape)
     test_image = cv2.imread(f'{data.main_dir}/{data.total_imgs[0]}')
     test_image_resized = cv2.resize(test_image, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
+    test_image_normalized = cv2.normalize(test_image_resized, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+    test_image_rgb_normalized = (test_image_resized - np.min(test_image_resized))/np.ptp(test_image_resized)
     print(test_image.shape)
     print(test_image_resized.shape)
+    # print(test_image_resized[:3][:3][:3])
+    # print(test_image_normalized[:3][:3][:3])
+    print(test_image_rgb_normalized)
     plt.imshow(test_image)
     plt.show()
     plt.imshow(test_image_resized)
     plt.show()
+    plt.imshow(test_image_normalized)
+    plt.show()
+    plt.imshow(test_image_resized)
+    plt.show()
 
+    loader = DataLoader(data,batch_size=12, num_workers=2)
+    for i, batch in enumerate(loader):
+        print(i, batch)
