03ae005ac6f4f9cef3059a716e7f3a75e389939a
Ren You <roadtoti10@gmail.com>
2022-09-09 03:34:02 +0930

    training loop tests

--- classes.py
+++ classes.py
@@ -5,19 +5,57 @@ from torchvision import datasets, transforms
 from PIL import Image
 import os
 import natsort
+import glob
+from typing import List, Tuple
+from torchvision.io import read_image
+import torchvision.transforms.functional as func
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, main_dir):
-        # 'Initialization'
+    def __init__(self, main_dir, transform=None):
         self.main_dir = main_dir
-        # self.transform = transform
-        self.total_imgs = natsort.natsorted(os.listdir(main_dir))
-        self.list_IDs = []
+        self.img_paths = [path for path in glob.glob(main_dir+"*.jpg", recursive=True)]
+        self.transform = transform
 
     def __len__(self):
-        return len(self.total_imgs)
+        return len(self.img_paths)
 
     def __getitem__(self, index):
-        return self.total_imgs[index]
+        return self.preprocess(index)
+
+    def preprocess(self, index) -> Tuple:
+        if index < 0:
+            index = 0
+        if index >= len(self):
+            index = len(self)-1
+        image = read_image(self.img_paths[index])
+        if self.transform:
+            image = self.transform(image)
+        image = func.resize(image, [64,64])
+        return (image,index)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
 
--- pytorch_script.py
+++ pytorch_script.py
@@ -1,15 +1,16 @@
 import torch
 import matplotlib.pyplot as plt
-from torchvision import datasets, transforms
-from classes import Dataset
-import numpy as np
-from PIL import Image
-import cv2
+# from torchvision import datasets, transforms
+from classes import Dataset, Net
+# import numpy as np
+# from PIL import Image
+# import cv2
 # import pytorch_lightning as pl
 from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
+# from torchvision.datasets import MNIST
+import torch.optim as optim
 
 
 if __name__ == "__main__":
@@ -19,36 +20,45 @@ if __name__ == "__main__":
     # torch.backends.cudnn.benchmark = True
 
     # Parameters
-    # params = {'batch_size': 64,
-    #           'shuffle': True,
-    #           'num_workers': 6}
-    # max_epochs = 100
+    params = {'batch_size': 64,
+              'shuffle': True,
+              'num_workers': 6}
+    max_epochs = 1
 
     # Datasets
-    data = Dataset("data/Raphael")
-    # imageset = np.empty(shape=len(data))
-    # for i,v in enumerate(data.total_imgs):
-    #     imageset[i] = np.array(Image.open(f'{data.main_dir}/{v}'))
-    # print(imageset)
-    # print(imageset.shape)
-    test_image = cv2.imread(f'{data.main_dir}/{data.total_imgs[0]}')
-    test_image_resized = cv2.resize(test_image, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
-    test_image_normalized = cv2.normalize(test_image_resized, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
-    test_image_rgb_normalized = (test_image_resized - np.min(test_image_resized))/np.ptp(test_image_resized)
-    print(test_image.shape)
-    print(test_image_resized.shape)
-    # print(test_image_resized[:3][:3][:3])
-    # print(test_image_normalized[:3][:3][:3])
-    print(test_image_rgb_normalized)
-    plt.imshow(test_image)
-    plt.show()
-    plt.imshow(test_image_resized)
-    plt.show()
-    plt.imshow(test_image_normalized)
-    plt.show()
-    plt.imshow(test_image_resized)
+    dataset = Dataset("data/Raphael/")
+    test_image = dataset[0]
+    print(test_image)
+    plt.imshow(test_image[0].permute(1,2,0))
     plt.show()
 
-    loader = DataLoader(data,batch_size=12, num_workers=2)
-    for i, batch in enumerate(loader):
-        print(i, batch)
+    # loader = DataLoader(data,batch_size=12, num_workers=2)
+    # for i, batch in enumerate(loader):
+    #     print(i, batch)
+    trainloader = torch.utils.data.DataLoader(dataset, batch_size=16,shuffle=True, num_workers=2)
+    criterion = nn.CrossEntropyLoss()
+    net = Net()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
